e392585ede29bd64105b733fd0198fda8f35b870
许雄龙 <xionglong.xu@gmail.com>
2019-03-04 23:04:12 +0800

    update

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ function loadDefaultScene()
             _scene = gltf.scene;
             _scene.background = new THREE.Color(0xf0f0f0);
             _camera = gltf.cameras[0];
+            //_cameraControl = new THREE.OrbitControls(_camera, _renderer.domElement);
             _debugDrawer = new THREE.AmmoDebugDrawer(_scene, _physicsWorld);
             _debugDrawer.enable();
             resize();
@@ -69,19 +70,21 @@ function loadDefaultScene()
             
         },
         function(err) {
+
         }
     );
 }
 
 function createPhysicsScene(scene, jsonData)
 {
+    let rigidbodyMap = new Map();
     jsonData.rigid_bodys.forEach(function(rigidBodyData) {
         let object = scene.getObjectByName(rigidBodyData.name.replace(/\./g, ""));
         if(object !== undefined && object instanceof THREE.Mesh) {
             //let worldScale = new THREE.Vector3();
             //object.getWorldScale(worldScale);
             let geometry = object.geometry;
-            let shape;
+            let shape = null;
             switch(rigidBodyData.collision_shape) {
                 case "BOX":
                 {
@@ -110,19 +113,95 @@ function createPhysicsScene(scene, jsonData)
                     break;
                 }
                 default:
-                {
-                    break;
-                }
             }
-            if(shape !== undefined) {
+            if(shape !== null) {
                 let rigidBody = createRigidBody(object, shape, rigidBodyData.mass);
                 if(rigidBodyData.kinematic) {
-
+                    rigidBody.setCollisionFlags(rigidBody.getCollisionFlags() | 2);
                 }
+                rigidbodyMap.set(rigidBodyData.name, rigidBody);
             }
 
         }
 
+    });
+
+    jsonData.constraints.forEach(function(constraintData) {
+        let objectA = constraintData.object1 !== undefined ? rigidbodyMap.get(constraintData.object1) : null;
+        let objectB = constraintData.object2 !== undefined ? rigidbodyMap.get(constraintData.object2) : null;
+        if(objectA === null && objectB !== null) {
+            objectA = objectB;
+            objectB = null;
+        }
+        if(objectA !== null) {
+            let constraint = null;
+            let translationOffsetA = constraintData.translation_offset_a;
+            let pivotA = new Ammo.btVector3(translationOffsetA[0], translationOffsetA[1], translationOffsetA[2]);
+            let rotationOffsetA = new THREE.Quaternion();
+            rotationOffsetA.fromArray(constraintData.rotation_offset_a);
+
+
+            let pivotB;
+            let rotationOffsetB;
+            if(objectB !== null) {
+                let translationOffsetB = constraintData.translation_offset_b;
+                pivotB = new Ammo.btVector3(translationOffsetB[0], translationOffsetB[1], translationOffsetB[2]);
+                rotationOffsetB = new THREE.Quaternion();
+                rotationOffsetB.fromArray(constraintData.rotation_offset_b);
+            }
+            switch(constraintData.type) {
+                case "FIXED":
+                {
+                    
+                    break;
+                }
+                case "POINT":
+                {
+                    if(objectB === null) {
+                        constraint = new Ammo.btPoint2PointConstraint(objectA, pivotA);
+                    }
+                    else {
+                        constraint = new Ammo.btPoint2PointConstraint(objectA, objectB, pivotA, pivotB);
+                    }
+                    break;
+                }
+                case "HINGE":
+                {
+                    let axisA = new THREE.Vector3(0, 0, 1);
+                    axisA.applyQuaternion(rotationOffsetA);
+
+                    if(objectB === null) {
+                        constraint = new Ammo.btHingeConstraint(objectA, pivotA, new Ammo.btVector3(axisA.x, axisA.y, axisA.z));
+                    }
+                    else {
+                        let axisB = new THREE.Vector3(0, 0, 1);
+                        axisB.applyQuaternion(rotationOffsetB);
+                        constraint = new Ammo.btHingeConstraint(objectA, objectB, pivotA, pivotB, new Ammo.btVector3(axisA.x, axisA.y, axisA.z), new Ammo.btVector3(axisB.x, axisB.y, axisB.z));
+                    }
+                    break;
+                }
+                case "SLIDER":
+                {
+                    break;
+                }
+                case "PISTON":
+                {
+                    break;
+                }
+                case "GENERIC":
+                {
+                    break;
+                }
+                case "GENERIC_SPRING":
+                {
+                    break;
+                }
+                default:
+            }
+            if(constraint !== null) {
+                _physicsWorld.addConstraint(constraint, constraintData.disable_collisions);
+            }
+        }
     });
 }
 
